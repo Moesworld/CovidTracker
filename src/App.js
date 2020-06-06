0ba19922b2597d250750e859834a64ab8093897d
Moe Hirzalla <mohammed.hirzalla@hotmail.com>
2020-06-06 08:34:27 -0400

    connected App to API using axios

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 
 import { Cards, Charts, CountryPicker} from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
+
+
 class App extends React.Component{
+
+  state ={
+    data: {},
+  }
+   
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data: fetchData});
+    console.log(fetchData)
+  }
   render(){
+    const { data } = this.state;
     return(
     <div className={styles.container}> 
       <Cards />
