8f9f225cc124050190477b57fb4eb7681f87d8cb
Moe Hirzalla <mohammed.hirzalla@hotmail.com>
2020-06-06 08:09:08 -0400

    initializing components and includeing some css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
+import { Cards, Charts, CountryPicker} from './components';
+import styles from './App.module.css';
 class App extends React.Component{
   render(){
-    return(<div> <h1>APP</h1> </div>)
+    return(
+    <div className={styles.container}> 
+      <Cards />
+      <Charts />
+      <CountryPicker/>
+    </div>
+    )
   }
 }
 export default App;
